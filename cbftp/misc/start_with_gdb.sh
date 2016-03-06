7f3d3b9add79c0c017423a1e0974400b4c54dc42
steakboy <steakboy@851a5ac4-13fe-4265-9a9c-444b56a01f7e>
2016-03-06 00:48:38 +0000

    updated gdb script to let the new signal pass through

--- cbftp/misc/start_with_gdb.sh
+++ cbftp/misc/start_with_gdb.sh
@@ -2,4 +2,4 @@
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd`
 popd > /dev/null
-gdb $SCRIPTPATH/cbftp --eval-command="set confirm off" --eval-command="handle SIGINT nostop print pass" --eval-command="set confirm on" --eval-command="run"
+gdb $SCRIPTPATH/cbftp --eval-command="set confirm off" --eval-command="handle SIGINT nostop print pass" --eval-command="handle SIGUSR1 nostop noprint pass" --eval-command="set confirm on" --eval-command="run"
