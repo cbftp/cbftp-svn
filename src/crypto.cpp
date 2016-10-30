6c5c61af799ea16a6245e3537e8451a943a19080
steakboy7000 <52637758+steakboy7000@users.noreply.github.com>
2016-10-30 15:33:39 +0000

    updated usage of EVP_CIPHER_CTX due to changes in openssl 1.1.0

--- src/crypto.cpp
+++ src/crypto.cpp
@@ -18,8 +18,7 @@ int blockSize() {
 }
 
 void Crypto::encrypt(const BinaryData & indata, const BinaryData & key, BinaryData & outdata) {
-  EVP_CIPHER_CTX ctx;
-  EVP_CIPHER_CTX_init(&ctx);
+  EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
   const EVP_CIPHER * cipherp = cipher();
   int ivlen = EVP_CIPHER_iv_length(cipherp);
   outdata.resize(ivlen + indata.size() + blockSize());
@@ -29,26 +28,25 @@ void Crypto::encrypt(const BinaryData & indata, const BinaryData & key, BinaryDa
   for (int i = 0; i < ivlen; i++) {
     outdata[i] = (unsigned char)(rand() % UCHAR_MAX);
   }
-  EVP_EncryptInit_ex(&ctx, cipherp, NULL, &key[0], &outdata[0]);
-  EVP_EncryptUpdate(&ctx, &outdata[ivlen], &resultlen, &indata[0], indata.size());
-  EVP_EncryptFinal_ex(&ctx, &outdata[ivlen+resultlen], &finallen);
+  EVP_EncryptInit_ex(ctx, cipherp, NULL, &key[0], &outdata[0]);
+  EVP_EncryptUpdate(ctx, &outdata[ivlen], &resultlen, &indata[0], indata.size());
+  EVP_EncryptFinal_ex(ctx, &outdata[ivlen+resultlen], &finallen);
   outdata.resize(ivlen + resultlen + finallen);
-  EVP_CIPHER_CTX_cleanup(&ctx);
+  EVP_CIPHER_CTX_free(ctx);
 }
 
 void Crypto::decrypt(const BinaryData & indata, const BinaryData & key, BinaryData & outdata) {
-  EVP_CIPHER_CTX ctx;
-  EVP_CIPHER_CTX_init(&ctx);
+  EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
   const EVP_CIPHER * cipherp = cipher();
   int ivlen = EVP_CIPHER_iv_length(cipherp);
   outdata.resize(indata.size() + blockSize());
   int writelen;
   int finalwritelen;
-  EVP_DecryptInit_ex(&ctx, cipherp, NULL, &key[0], &indata[0]);
-  EVP_DecryptUpdate(&ctx, &outdata[0], &writelen, &indata[ivlen], indata.size() - ivlen);
-  EVP_DecryptFinal_ex(&ctx, &outdata[writelen], &finalwritelen);
+  EVP_DecryptInit_ex(ctx, cipherp, NULL, &key[0], &indata[0]);
+  EVP_DecryptUpdate(ctx, &outdata[0], &writelen, &indata[ivlen], indata.size() - ivlen);
+  EVP_DecryptFinal_ex(ctx, &outdata[writelen], &finalwritelen);
   outdata.resize(writelen + finalwritelen);
-  EVP_CIPHER_CTX_cleanup(&ctx);
+  EVP_CIPHER_CTX_free(ctx);
 }
 
 void Crypto::sha256(const BinaryData & indata, BinaryData & outdata) {
