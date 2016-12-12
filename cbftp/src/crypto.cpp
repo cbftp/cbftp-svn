81239d567213b7dd2940499bcff35a5dff630d66
steakboy <steakboy@851a5ac4-13fe-4265-9a9c-444b56a01f7e>
2016-12-12 20:48:08 +0000

    improved data file encryption method, now compatible with openssl enc for refeference, the data file can now be decrypted with: openssl enc -d -aes-256-cbc -md sha256 -in ~/.cbftp/data

--- cbftp/src/crypto.cpp
+++ cbftp/src/crypto.cpp
@@ -2,13 +2,21 @@
 
 #include <climits>
 #include <ctime>
+#include <cstring>
 #include <openssl/sha.h>
 #include <openssl/evp.h>
+
+#define SALT_LENGTH 8
+#define SALT_STRING_LENGTH 16
 
 namespace {
 
 const EVP_CIPHER * cipher() {
   return EVP_aes_256_cbc();
+}
+
+const EVP_MD * digest() {
+  return EVP_sha256();
 }
 
 int blockSize() {
@@ -17,25 +25,55 @@ int blockSize() {
 
 }
 
-void Crypto::encrypt(const BinaryData & indata, const BinaryData & key, BinaryData & outdata) {
+void Crypto::encrypt(const BinaryData & indata, const BinaryData & pass, BinaryData & outdata) {
   EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
   const EVP_CIPHER * cipherp = cipher();
   int ivlen = EVP_CIPHER_iv_length(cipherp);
-  outdata.resize(ivlen + indata.size() + blockSize());
-  int resultlen;
-  int finallen;
+  int keylen = EVP_CIPHER_key_length(cipherp);
+  unsigned char * key = (unsigned char *) malloc(keylen);
+  unsigned char * iv = (unsigned char *) malloc(ivlen);
+  outdata.resize(SALT_STRING_LENGTH + indata.size() + blockSize());
+  memcpy(&outdata[0], "Salted__", SALT_STRING_LENGTH - SALT_LENGTH);
   srand(time(NULL));
-  for (int i = 0; i < ivlen; i++) {
+  for (int i = SALT_LENGTH; i < SALT_STRING_LENGTH; ++i) {
     outdata[i] = (unsigned char)(rand() % UCHAR_MAX);
   }
-  EVP_EncryptInit_ex(ctx, cipherp, NULL, &key[0], &outdata[0]);
-  EVP_EncryptUpdate(ctx, &outdata[ivlen], &resultlen, &indata[0], indata.size());
-  EVP_EncryptFinal_ex(ctx, &outdata[ivlen+resultlen], &finallen);
-  outdata.resize(ivlen + resultlen + finallen);
+  EVP_BytesToKey(cipherp, digest(), &outdata[SALT_STRING_LENGTH - SALT_LENGTH], &pass[0],
+                 pass.size(), 1, key, iv);
+  int resultlen;
+  int finallen;
+  EVP_EncryptInit_ex(ctx, cipherp, NULL, key, iv);
+  EVP_EncryptUpdate(ctx, &outdata[SALT_STRING_LENGTH], &resultlen, &indata[0], indata.size());
+  EVP_EncryptFinal_ex(ctx, &outdata[SALT_STRING_LENGTH + resultlen], &finallen);
+  outdata.resize(SALT_STRING_LENGTH + resultlen + finallen);
   EVP_CIPHER_CTX_free(ctx);
+  free(key);
+  free(iv);
 }
 
-void Crypto::decrypt(const BinaryData & indata, const BinaryData & key, BinaryData & outdata) {
+void Crypto::decrypt(const BinaryData & indata, const BinaryData & pass, BinaryData & outdata) {
+  EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
+  const EVP_CIPHER * cipherp = cipher();
+  int ivlen = EVP_CIPHER_iv_length(cipherp);
+  int keylen = EVP_CIPHER_key_length(cipherp);
+  unsigned char * key = (unsigned char *) malloc(keylen);
+  unsigned char * iv = (unsigned char *) malloc(ivlen);
+  EVP_BytesToKey(cipherp, digest(), &indata[SALT_STRING_LENGTH - SALT_LENGTH], &pass[0],
+                 pass.size(), 1, key, iv);
+  outdata.resize(indata.size() + blockSize());
+  int writelen;
+  int finalwritelen;
+  EVP_DecryptInit_ex(ctx, cipherp, NULL, key, iv);
+  EVP_DecryptUpdate(ctx, &outdata[0], &writelen, &indata[SALT_STRING_LENGTH],
+                    indata.size() - SALT_STRING_LENGTH);
+  EVP_DecryptFinal_ex(ctx, &outdata[writelen], &finalwritelen);
+  outdata.resize(writelen + finalwritelen);
+  EVP_CIPHER_CTX_free(ctx);
+  free(key);
+  free(iv);
+}
+
+void Crypto::decryptOld(const BinaryData & indata, const BinaryData & key, BinaryData & outdata) {
   EVP_CIPHER_CTX * ctx = EVP_CIPHER_CTX_new();
   const EVP_CIPHER * cipherp = cipher();
   int ivlen = EVP_CIPHER_iv_length(cipherp);
