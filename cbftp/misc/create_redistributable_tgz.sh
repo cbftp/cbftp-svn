90eefa01c372110953a3c7433fda008fb6d88aa6
steakboy <steakboy@851a5ac4-13fe-4265-9a9c-444b56a01f7e>
2017-05-22 15:41:41 +0000

    updated redistribution script to add readme

--- cbftp/misc/create_redistributable_tgz.sh
+++ cbftp/misc/create_redistributable_tgz.sh
@@ -9,6 +9,6 @@ fi
 VERSION=`svn info|grep Revision|awk '{ print $2 }'`
 echo $VERSION > .version
 VERSION="r"$VERSION
-FILES=`find . -name "*.cpp" -o -name "*.h" -o -name Makefile -o -name Makefile.inc -o -name .version -o -name start_with_gdb.sh|xargs`
+FILES=`find . -name "*.cpp" -o -name "*.h" -o -name Makefile -o -name Makefile.inc -o -name .version -o -name start_with_gdb.sh -o -name README|xargs`
 tar czf cbftp-$VERSION.tar.gz $FILES --transform "s,^\.,cbftp-$VERSION,"
 rm .version
