fd503b6b2ab53d391387b2f96dbb48ab1ec32851
steakboy7000 <52637758+steakboy7000@users.noreply.github.com>
2019-08-24 11:22:28 +0000

    fixed a crash when clearing the base path field of a site

--- src/site.cpp
+++ src/site.cpp
@@ -529,7 +529,7 @@ void Site::setPass(const std::string & pass) {
 }
 
 void Site::setBasePath(const std::string & basepath) {
-  this->basepath = basepath;
+  this->basepath = basepath.empty() ? "/" : basepath;
 }
 
 void Site::setMaxLogins(unsigned int num) {
