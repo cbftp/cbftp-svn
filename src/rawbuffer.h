c731545e8782a7640d142d055691370860d5731d
steakboy7000 <52637758+steakboy7000@users.noreply.github.com>
2019-10-16 11:19:11 +0000

    added filtering support in the event log

--- src/rawbuffer.h
+++ src/rawbuffer.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <list>
+#include <regex>
 #include <string>
 #include <vector>
 #include <utility>
@@ -10,10 +12,17 @@ class RawBuffer {
   private:
     void lineFinished();
     void addLogText(const std::string& tag, const std::string& text);
-    std::vector<std::pair<std::string, std::string> > log;
-    std::vector<std::pair<std::string, std::string> > logcopy;
+    void applyFilters();
+    void applyFilters(bool copy);
+    void addFilterLine(const std::pair<std::string, std::string>& line, std::vector<std::pair<std::string, std::string>>& target, unsigned int& latest);
+    std::vector<std::pair<std::string, std::string>> log;
+    std::vector<std::pair<std::string, std::string>> logfiltered;
+    std::vector<std::pair<std::string, std::string>> logcopy;
+    std::vector<std::pair<std::string, std::string>> logcopyfiltered;
     unsigned int latestp;
+    unsigned int latestpfiltered;
     unsigned int latestpcopy;
+    unsigned int latestpcopyfiltered;
     unsigned int maxlength;
     unsigned int bookmarklines;
     std::string site;
@@ -23,6 +32,9 @@ class RawBuffer {
     bool threads;
     bool eventlog;
     RawBufferCallback * callback;
+    std::list<std::string> wildcardfilters;
+    bool hasregexfilter;
+    std::regex regexfilter;
   public:
     RawBuffer(unsigned int, std::string, std::string);
     RawBuffer(std::string);
@@ -39,10 +51,22 @@ class RawBuffer {
     void rename(std::string);
     std::string getTag() const;
     unsigned int getSize() const;
+    unsigned int getFilteredSize() const;
     unsigned int getCopySize() const;
+    unsigned int getFilteredCopySize() const;
     const std::pair<std::string, std::string> & getLine(unsigned int) const;
     const std::pair<std::string, std::string> & getLineCopy(unsigned int) const;
+    const std::pair<std::string, std::string> & getFilteredLine(unsigned int) const;
+    const std::pair<std::string, std::string> & getFilteredLineCopy(unsigned int) const;
     void freezeCopy();
     void setUiWatching(bool watching);
     void clear();
+    bool isFiltered() const;
+    bool hasWildcardFilters() const;
+    bool hasRegexFilter() const;
+    std::list<std::string> getWildcardFilters() const;
+    std::regex getRegexFilter() const;
+    void setWildcardFilters(const std::list<std::string> & filters);
+    void setRegexFilter(const std::regex & filter);
+    void unsetFilters();
 };
--- src/ui/screens/eventlogscreen.cpp
+++ src/ui/screens/eventlogscreen.cpp
@@ -3,6 +3,7 @@
 #include "../../eventlog.h"
 #include "../../rawbuffer.h"
 #include "../../globalcontext.h"
+#include "../../util.h"
 
 #include "../ui.h"
 
@@ -16,43 +17,124 @@ void EventLogScreen::initialize(unsigned int row, unsigned int col) {
   rawbuf->setUiWatching(true);
   readfromcopy = false;
   copyreadpos = 0;
+  filtermodeinput = false;
+  filtermodeinputregex = false;
   init(row, col);
 }
 
 void EventLogScreen::redraw() {
+  ui->erase();
+  rawbuf->bookmark();
+  unsigned int rows = (filtermodeinput || filtermodeinputregex) ? row - 2 : row;
+  std::list<std::string> printlines;
+  bool negative = false;
+  if (rawbuf->isFiltered()) {
+    std::string ftext = filtertext;
+    negative = !ftext.empty() && ftext[0] == '!';
+    if (negative) {
+      ftext = filtertext.substr(1);
+    }
+  }
+  unsigned int size = rawbuf->isFiltered() ? rawbuf->getFilteredSize() : rawbuf->getSize();
+  if (readfromcopy) {
+    size = rawbuf->isFiltered() ? rawbuf->getFilteredCopySize() : rawbuf->getCopySize();
+  }
+  unsigned int numlinestoprint = size < rows ? size : rows;
+  for (unsigned int i = 0; i < numlinestoprint; ++i) {
+    unsigned int pos = numlinestoprint - i - 1;
+    const std::pair<std::string, std::string> & entry = readfromcopy ?
+        (rawbuf->isFiltered() ? rawbuf->getFilteredLineCopy(pos + copyreadpos) :
+        rawbuf->getLineCopy(pos + copyreadpos))
+     : (rawbuf->isFiltered() ? rawbuf->getFilteredLine(pos) : rawbuf->getLine(pos));
+    std::string line = entry.first + " " + entry.second;
+    ui->printStr(i, 0, line);
+  }
+  std::string oldtext = filterfield.getData();
+  filterfield = MenuSelectOptionTextField("filter", row - 1, 1, "", oldtext, col - 20, 512, false);
   update();
 }
 
 void EventLogScreen::update() {
-  ui->erase();
-  if (!readfromcopy) {
-    unsigned int numlinestoprint = rawbuf->getSize() < row ? rawbuf->getSize() : row;
-    for (unsigned int i = 0; i < numlinestoprint; i++) {
-      const std::pair<std::string, std::string> & entry = rawbuf->getLine(numlinestoprint - i - 1);
-      std::string line = entry.first + " " + entry.second;
-      ui->printStr(i, 0, line);
-    }
+  if (rawbuf->linesSinceBookmark()) {
+    redraw();
+    return;
   }
-  else {
-    unsigned int numlinestoprint = copysize < row ? copysize : row;
-    for (unsigned int i = 0; i < numlinestoprint; i++) {
-      const std::pair<std::string, std::string> & entry = rawbuf->getLineCopy(numlinestoprint - i - 1 + copyreadpos);
-      std::string line = entry.first + " " + entry.second;
-      ui->printStr(i, 0, line);
-    }
+  if (filtermodeinput || filtermodeinputregex) {
+    ui->showCursor();
+    std::string pretag = filtermodeinput ? "[Filter(s)]: " : "[Regex filter]: ";
+    ui->printStr(filterfield.getRow(), filterfield.getCol(), pretag + filterfield.getContentText());
+    ui->moveCursor(filterfield.getRow(), filterfield.getCol() + pretag.length() + filterfield.cursorPosition());
   }
 }
 
 bool EventLogScreen::keyPressed(unsigned int ch) {
+  if (filtermodeinput || filtermodeinputregex) {
+    if ((ch >= 32 && ch <= 126) || ch == KEY_BACKSPACE || ch == 8 || ch == 127 ||
+        ch == KEY_RIGHT || ch == KEY_LEFT || ch == KEY_DC || ch == KEY_HOME ||
+        ch == KEY_END)
+    {
+      filterfield.inputChar(ch);
+      ui->update();
+      return true;
+    }
+    else if (ch == 10) {
+      ui->hideCursor();
+      filtertext = filterfield.getData();
+      if (filtertext.length()) {
+        if (filtermodeinput) {
+          rawbuf->setWildcardFilters(util::trim(util::split(filtertext)));
+        }
+        else {
+          try {
+            std::regex regexfilter = util::regexParse(filtertext);
+            rawbuf->setRegexFilter(regexfilter);
+          }
+          catch (std::regex_error& e) {
+            ui->goInfo("Invalid regular expression.");
+            return true;
+          }
+        }
+        if (readfromcopy) {
+          copyreadpos = 0;
+        }
+      }
+      filtermodeinput = false;
+      filtermodeinputregex = false;
+      ui->redraw();
+      ui->setLegend();
+      return true;
+    }
+    else if (ch == 27) {
+      if (!filterfield.getData().empty()) {
+        filterfield.clear();
+        ui->update();
+      }
+      else {
+        filtermodeinput = false;
+        filtermodeinputregex = false;
+        ui->hideCursor();
+        ui->redraw();
+        ui->setLegend();
+      }
+      return true;
+    }
+    else if (ch == '\t') {
+      filtermodeinput = !filtermodeinput;
+      filtermodeinputregex = !filtermodeinputregex;
+      ui->update();
+      ui->setLegend();
+      return true;
+    }
+  }
   switch(ch) {
     case KEY_PPAGE:
       if (!readfromcopy) {
         rawbuf->freezeCopy();
         copyreadpos = 0;
-        copysize = rawbuf->getCopySize();
         readfromcopy = true;
       }
       else {
+        unsigned int copysize = rawbuf->isFiltered() ? rawbuf->getFilteredCopySize() : rawbuf->getCopySize();
         copyreadpos = copyreadpos + row / 2;
         if (row >= copysize) {
           copyreadpos = 0;
@@ -61,7 +143,7 @@ bool EventLogScreen::keyPressed(unsigned int ch) {
           copyreadpos = copysize - row;
         }
       }
-      ui->update();
+      ui->redraw();
       return true;
     case KEY_NPAGE:
       if (readfromcopy) {
@@ -75,11 +157,39 @@ bool EventLogScreen::keyPressed(unsigned int ch) {
           copyreadpos = copyreadpos - row / 2;
         }
       }
-      ui->update();
+      ui->redraw();
       return true;
-    case 10:
+    case 'f':
+      if (rawbuf->isFiltered()) {
+        rawbuf->unsetFilters();
+        ui->setInfo();
+        if (readfromcopy) {
+          copyreadpos = 0;
+        }
+      }
+      else {
+        filtermodeinput = true;
+        ui->setLegend();
+      }
+      ui->redraw();
+      return true;
+    case 'F':
+      if (rawbuf->isFiltered()) {
+        rawbuf->unsetFilters();
+        ui->setInfo();
+        if (readfromcopy) {
+          copyreadpos = 0;
+        }
+      }
+      else {
+        filtermodeinputregex = true;
+        ui->setLegend();
+      }
+      ui->redraw();
+      return true;
     case 27: // esc
       rawbuf->setUiWatching(false);
+      rawbuf->unsetFilters();
       ui->returnToLast();
       return true;
   }
@@ -87,9 +197,22 @@ bool EventLogScreen::keyPressed(unsigned int ch) {
 }
 
 std::string EventLogScreen::getLegendText() const {
-  return "[Pgup] Scroll up - [Pgdn] Scroll down - [ESC/Enter] Return";
+  if (filtermodeinput) {
+    return "[Any] Enter space separated filters. Valid operators are !, *, ?. Must match all negative filters and at least one positive if given. Case insensitive. - [Tab] switch mode - [Esc] Cancel";
+  }
+  if (filtermodeinputregex) {
+    return "[Any] Enter regex input - [Tab] switch mode - [Esc] Cancel";
+  }
+  return "[Pgup] Scroll up - [Pgdn] Scroll down - [ESC/Enter] Return - Toggle [f]iltering";
 }
 
 std::string EventLogScreen::getInfoLabel() const {
   return "EVENT LOG";
+}
+
+std::string EventLogScreen::getInfoText() const {
+  if (rawbuf->isFiltered()) {
+    return "FILTERING ON: " + filtertext;
+  }
+  return "";
 }
